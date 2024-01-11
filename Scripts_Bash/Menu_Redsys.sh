ee0c889aff81e0c97c2e31f40b547e590ae7a3b2
mtrigo99 <18682154+mtrigo99@users.noreply.github.com>
2024-01-11 21:43:51 +0100

    Add files via upload

--- Scripts_Bash/Menu_Redsys.sh
+++ Scripts_Bash/Menu_Redsys.sh
@@ -5,6 +5,20 @@ VERDE='\033[0;32m'
 AMARILLO='\e[0;33m'
 SIN_COLOR='\033[0m'
 
+archivo_win="wrstdrvs_pruebas.win"
+# Extraer los valores de STABLE_DEV_CODE y encontrar el máximo
+max_stable_dev_code=$(grep "STABLE_DEV_CODE" "$archivo_win" | cut -d'=' -f2 | sort -n | tail -n 1)
+max_stable_dev_code=$((max_stable_dev_code + 1))
+
+
+
+#Variables TPVs
+nombreTerminal #Nombre TPV
+NumPosto=0 #Puesto
+codigoComercio #Código de comercio
+codigoTerminal #Código de la terminal
+keyFirma #key_signature
+
 while true
 do
     clear
@@ -12,40 +26,63 @@ do
     echo -e "${ROJO}Configuración de Redsys"
     echo -e "-----------------------${SIN_COLOR}"
     echo " "
-    echo -e "${AMARILLO}1.${SIN_COLOR} Configurar la Máster"
-    echo -e "${AMARILLO}2.${SIN_COLOR} Configurar resto de TPVs"
-    echo -e "${AMARILLO}3.${SIN_COLOR} Comprobar ttyACM"
-    echo -e "${AMARILLO}4.${SIN_COLOR} Comprobar fallos log logDllImplantado.txt"
-    echo -e "${AMARILLO}5.${SIN_COLOR} Salir del menú"
+    echo -e "${AMARILLO}1.${SIN_COLOR} Copiar archivos"
+    echo -e "${AMARILLO}2.${SIN_COLOR} Configurar la Máster"
+    echo -e "${AMARILLO}3.${SIN_COLOR} Configurar resto de TPVs"
+    echo -e "${AMARILLO}4.${SIN_COLOR} Comprobar ttyACM"
+    echo -e "${AMARILLO}5.${SIN_COLOR} Comprobar pinpad"
+    echo -e "${AMARILLO}6.${SIN_COLOR} Revisar log logDllImplantado.txt"
+    echo -e "${AMARILLO}7.${SIN_COLOR} Salir del menú"
     echo " "
     echo "Por favor, selecciona una opción: "
     read opcion
 
     case $opcion in
+        
         1) 
-            while :
-            do
-                clear
-                echo "Submenú opción 1"
-                echo "1. SubOpción 1"cp
-                echo "2. SubOpción 2"
-                echo "3. Regresar al menú principal"
-                read subopcion
+            sudo chmod 777 Ficheros_Redsys/*
+            sudo cp -p Ficheros_Redsys/before_winrest /sbin
+            sudo cp -p Ficheros_Redsys/caTpvpcImplantado.pem /home/winrest
+            sudo cp -p Ficheros_Redsys/libImplantadoLinux.so /home/winrest
+            sudo cp -p Ficheros_Redsys/99-pinpad.rules /root/etc/udev/rules.d/ 
+            echo -e "${AMARILLO}Los archivos han sido copiados${SIN_COLOR}"
+            echo " "
+            read -p "Pulsa una tecla para continuar....." ;;
+        2)
+            echo -e "${AMARILLO}¿Nombre de la Máster?${SIN_COLOR}"
+            read nombreTerminal
+            echo -e "${AMARILLO}¿Código de comercio?${SIN_COLOR}"
+            read codigoComercio
+            echo -e "${AMARILLO}¿Código de terminal?${SIN_COLOR}"
+            read codigoTerminal
+            echo -e "${AMARILLO}¿Key_Signature?${SIN_COLOR}"
+            read keyFirma
+            
+echo "
+[0B150001] 
+DEV_ID=0X0B150001
+STABLE_DEV_CODE=$max_stable_dev_code
+NAME=Redsys $nombreTerminal 
+POSTO=1
+COMMERCE_CODE=$codigoComercio
+TERMINAL_CODE=$codigoTerminal
+KEY_SIGNATURE=$keyFirma
+CONF_PINPAD=/dev/pinpad0,19200,N,8,1
+VERSION=8.1
+PRINT_CLIENT_RECEIPT_ON_INVOICE=1
+AUTO_REFUND=1" >> wrstdrvs_pruebas.win
 
-                case $subopcion in
-                    1) echo "Subopción 1 pulsada";;
-                    2) echo "Subopción 2 pulsada";;
-                    3) break ;;
+nano wrstdrvs_pruebas.win
 
-                esac
-            done ;;
+read -p "Pulsa una tecla para continuar....." ;;
 
-        2) echo "Has seleccionado la opción 2";;
-        3) ls /dev/ttyACM*
+        3) read -p "Pulsa una tecla para continuar...." ;;
+        4) ls -lah /dev/ttyACM*
            read -p "Pulsa una tecla para continuar...." ;;
-        4) cat /tmp/logDllImplantado.txt
+        5) read -p "Pulsa una tecla para continuar...." ;;
+        6) cat /tmp/logDllImplantado.txt
            read -p "Pulsa una tecla para continuar...." ;;
-        5) break ;;
+        7) clear; break ;;
         *) echo "Opción no válida";;
     esac
 done
