537f90b55901d93f7c0158417c5b6872061651c1
mtrigo99 <18682154+mtrigo99@users.noreply.github.com>
2024-01-12 20:20:05 +0100

    Add files via upload

---
+++ Crear_iteracion_pruebas.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#buscador dev_code
+archivo_win="wrstdrvs_pruebas.win"
+# Extraer los valores de STABLE_DEV_CODE y encontrar el máximo
+max_stable_dev_code=$(grep "STABLE_DEV_CODE" "$archivo_win" | cut -d'=' -f2 | sort -n | tail -n 1)
+max_stable_dev_code=$((max_stable_dev_code + 1))
+
+# Pedir al usuario que ingrese un número
+read -p "¿Cuántas TPVs hay que configurar?: " num
+read -p "¿En qué puesto debe comenzar la TPV Máster?: " num2
+
+# Texto a repetir
+#[0B150001] Para saber la estructura (Este itera)
+texto="DEV_ID=0x0B150001"
+#STABLE_DEV_CODE=84 Para saber la estructura. El número lo coge de función
+texto2="NAME=Redsys TPV1"
+#"POSTO=" Lo dejo para saber la estructura (Este itera)
+texto3="COMMERCE_CODE=49524846
+TERMINAL_CODE=9
+KEY_SIGNATURE=cf3203c284cb5e7f3382
+CONF_PINPAD=/dev/ttyACM0,19200,N,8,1
+VERSION=8.1
+PRINT_CLIENT_RECEIPT_ON_INVOICE=1
+AUTO_REFUND=1"
+
+# Bucle para repetir el texto el número de veces ingresado
+for ((i=1; i<=$num; i++))
+do
+    echo -e "[0B15000$i]"
+    echo -e "$texto"
+    echo -e "STABLE_DEV_CODE=$max_stable_dev_code"
+
+    # Imprimir "POSTO=" una vez y luego incrementar su valor
+    echo -e "POSTO=$num2"
+    ((num2++))
+
+    echo -e "$texto2"
+    echo -e "$texto3"
+    
+    echo ""
+    ((max_stable_dev_code++))
+done
